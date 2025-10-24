f5359a1a87de05dc6e9e5bb449178d8f436f0bce
melancholyano <hanemono.groupa@gmail.com>
2025-10-24 20:29:35 +0900

    aaa

--- backend/src/main/java/com/example/backend/HelloController.java
+++ backend/src/main/java/com/example/backend/HelloController.java
@@ -2,8 +2,10 @@ package com.example.backend;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class HelloController {
 
     @GetMapping("/api/message")
