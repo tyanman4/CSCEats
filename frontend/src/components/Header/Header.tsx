244ba93c8a67cdb45a84ad08e17bec5df11cdf85
melancholyano <hanemono.groupa@gmail.com>
2025-11-23 13:05:05 +0900

    ヘッダーレイアウトいじる

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -15,9 +15,9 @@ export const Header: React.FC = () => {
       <div className={styles.right}>
 
         {isAuthenticated ? <p>ようこそ、{user?.name}さん</p> : <p>ゲストとして利用中</p>}
-        {!isAuthenticated && location.pathname !== "/login" && <button onClick={() => navigate("/login")}>ログインページへ</button>}
-        {location.pathname !== "/mypage" && isAuthenticated && <button onClick={() => navigate("/mypage")}>マイページ</button>}
-        {isAuthenticated && <button onClick={logout}>ログアウト</button>}
+        {!isAuthenticated && location.pathname !== "/login" && <button className={styles.button} onClick={() => navigate("/login")}>ログイン</button>}
+        {location.pathname !== "/mypage" && isAuthenticated && <button className={styles.button} onClick={() => navigate("/mypage")}>マイページ</button>}
+        {isAuthenticated && <button onClick={logout} className={styles.button}>ログアウト</button>}
       </div>
     </div>
   );
