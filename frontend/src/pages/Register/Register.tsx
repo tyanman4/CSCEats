98b9ceae13f46c969ff87887aed099f960672f9a
melancholyano <hanemono.groupa@gmail.com>
2025-12-21 14:54:20 +0900

    新規登録時のバリデーション強化

--- frontend/src/pages/Register/Register.tsx
+++ frontend/src/pages/Register/Register.tsx
@@ -33,13 +33,18 @@ export const Register: React.FC = () => {
       return;
     }
 
-    if (formData.name.length < 2) {
-      setMessage("ユーザ名は2文字以上にしてください。");
+    //空白や全角英数字は不可
+    const usernameRegex = /^[a-zA-Z0-9ぁ-んァ-ヶ一-龠々・ー_-]{2,}$/;
+
+    if (!usernameRegex.test(formData.name)) {
+      setMessage("ユーザ名は2文字以上で、使用できない文字を含めないでください。");
       return;
     }
 
-    if (formData.password.length < 4) {
-      setMessage("パスワードは4文字以上にしてください。");
+    const passwordRegex = /^[\x21-\x7E]{4,}$/;
+
+    if (!passwordRegex.test(formData.password)) {
+      setMessage("パスワードは4文字以上で、英数字・記号（半角）を使用してください。");
       return;
     }
 
@@ -53,7 +58,7 @@ export const Register: React.FC = () => {
       const response = await appApi.post("/save", formData);
       if (response.status === 200) {
         login(response.data.token);
-        navigate(response.data.redirect)
+        navigate("/restaurants")
       } else {
         setMessage("登録に失敗しました。");
       }
