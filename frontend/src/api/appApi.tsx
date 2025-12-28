9c4fc314bbac54470740892d67c919b5fbd934f9
tyanman4 <pbb4g7zhfb@gmail.com>
2025-12-28 17:29:55 +0900

    パス変更

--- frontend/src/api/appApi.tsx
+++ frontend/src/api/appApi.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const appApi = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://csceats.onrender.com//api",
   headers: {
     "Content-Type": "application/json",
   },
