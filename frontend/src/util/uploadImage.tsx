21e7caa75dd4af635ad3345732365dbafc1550ea
tyanman4 <pbb4g7zhfb@gmail.com>
2026-01-19 19:29:24 +0900

    feat: add firebase storage API

---
+++ frontend/src/util/uploadImage.tsx
@@ -0,0 +1,14 @@
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { storage } from "../firebase";
+
+export const uploadImage = async (file: File, restaurantId: number) => {
+  const fileRef = ref(
+    storage,
+    `restaurants/${restaurantId}/${Date.now()}_${file.name}`
+  );
+
+  await uploadBytes(fileRef, file);
+  const url = await getDownloadURL(fileRef);
+
+  return url; // ← これを backend に送る
+};
